47e80b93790aaf18ab7db1331da0203704520f73
Aditya <thebeaditya@gmail.com>
2023-05-27 12:08:32 +0545

    Updated plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,12 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telscope" },
+    },
+  },
 }
