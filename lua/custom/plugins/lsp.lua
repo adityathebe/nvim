5f6563c915b5e486976c7a2d7cc8fc7e8fc7a5b6
Aditya Thebe <contact@adityathebe.com>
2024-10-12 19:43:13 +0545

    chore: config updates

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -75,15 +75,15 @@ return {
         -- Jump to the type of the word under your cursor.
         --  Useful when you're not sure what type a variable is and you want to see
         --  the definition of its *type*, not where it was *defined*.
-        map('<leader>lD', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+        map('gt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype')
 
         -- Fuzzy find all the symbols in your current document.
         --  Symbols are things like variables, functions, types, etc.
-        map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+        map('<leader>lsd', require('telescope.builtin').lsp_document_symbols, '[S]ymbols [D]ocument')
 
         -- Fuzzy find all the symbols in your current workspace.
         --  Similar to document symbols, except searches over your entire project.
-        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+        map('<leader>lsw', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[S]ymbols [W]orkspace')
 
         -- Rename the variable under your cursor.
         --  Most Language Servers support renaming across files, etc.
@@ -92,10 +92,6 @@ return {
         -- Execute a code action, usually your cursor needs to be on top of an error
         -- or a suggestion from your LSP for this to activate.
         map('<leader>lc', vim.lsp.buf.code_action, '[C]ode action', { 'n', 'x' })
-
-        -- WARN: This is not Goto Definition, this is Goto Declaration.
-        --  For example, in C this would take you to the header.
-        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
         -- The following two autocommands are used to highlight references of the
         -- word under your cursor when your cursor rests there for a little while.
@@ -140,11 +136,11 @@ return {
         --
         -- This may be unwanted, since they displace some of your code
         if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-          map('<leader>th', function()
+          map('<leader>lh', function()
             vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled {
               bufnr = event.buf,
             })
-          end, '[T]oggle Inlay [H]ints')
+          end, 'Inlay [H]ints')
         end
       end,
     })
--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -5,6 +5,7 @@ return {
     icons = {
       -- set icon mappings to true if you have a Nerd Font
       mappings = vim.g.have_nerd_font,
+
       -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
       -- default whick-key.nvim defined Nerd Font icons, otherwise define a string table
       keys = vim.g.have_nerd_font and {} or {
@@ -44,7 +45,8 @@ return {
       { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
       { '<leader>d', group = '[D]ocument' },
       { '<leader>r', group = '[R]ename' },
-      { '<leader>s', group = '[S]nd' },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>l', group = '[L]SP' },
       { '<leader>w', group = '[W]orkspace' },
       { '<leader>t', group = '[T]oggle' },
       { '<leader>g', group = '[G]itSigns', mode = { 'n', 'v' } },
