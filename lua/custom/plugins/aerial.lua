3f12474004e12fccb3429cb46432c0dd26a153f9
Aditya Thebe <contact@adityathebe.com>
2024-10-17 11:55:25 +0545

    chore: try out nighowl theme and increase width for aerial

--- lua/custom/plugins/aerial.lua
+++ lua/custom/plugins/aerial.lua
@@ -1,6 +1,13 @@
 return {
   'stevearc/aerial.nvim',
-  opts = {},
+  opts = {
+    layout = {
+      -- min_width and max_width can be a list of mixed types.
+      -- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
+      max_width = { 60, 0.2 },
+      min_width = 50,
+    },
+  },
   -- Optional dependencies
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
--- lua/custom/plugins/color-scheme.lua
+++ lua/custom/plugins/color-scheme.lua
@@ -13,10 +13,19 @@ return {
   --     vim.cmd 'colorscheme nord'
   --   end,
 
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  priority = 1000,
+  -- 'catppuccin/nvim',
+  -- name = 'catppuccin',
+  -- priority = 1000,
+  -- config = function()
+  --   vim.cmd.colorscheme 'catppuccin-mocha'
+  -- end,
+
+  'oxfist/night-owl.nvim',
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    vim.cmd.colorscheme 'catppuccin-mocha'
+    -- load the colorscheme here
+    require('night-owl').setup()
+    vim.cmd.colorscheme 'night-owl'
   end,
 }
