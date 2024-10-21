5ebed71dcd6dcae8c048d2c7dc177e1e754b360a
Aditya Thebe <contact@adityathebe.com>
2024-10-21 09:58:38 +0545

    noice.nvim install

---
+++ lua/custom/plugins/noice.lua
@@ -0,0 +1,21 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    presets = {
+      bottom_search = false, -- use a classic bottom cmdline for search
+      command_palette = true, -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      inc_rename = false, -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = false, -- add a border to hover docs and signature help
+    },
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+    }
+}
