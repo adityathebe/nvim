7701fbfdfd0bf007041de3b6db9f4669a9df9790
Aditya Thebe <contact@adityathebe.com>
2024-10-23 22:19:45 +0545

    chore: imrpove lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,3 +1,14 @@
+local function diff_source()
+  local gitsigns = vim.b.gitsigns_status_dict
+  if gitsigns then
+    return {
+      added = gitsigns.added,
+      modified = gitsigns.changed,
+      removed = gitsigns.removed
+    }
+  end
+end
+
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -7,12 +18,41 @@ return {
       globalstatus = true,
     },
     sections = {
-      lualine_a = { 'mode', 'buffers' },
-      lualine_b = { 'branch', 'diff', 'diagnostics' },
-      lualine_c = { { 'filename', path = 1 } },
-      lualine_x = { 'encoding', 'fileformat', 'filetype' },
+      lualine_a = { 'mode' },
+      lualine_b = {
+        'branch',
+        {
+          'diff',
+          source = diff_source
+        },
+        'diagnostics'
+      },
+      lualine_c = {
+        {
+          'filename',
+          path = 1, -- fullpath
+        },
+      },
+      lualine_x = {
+        'encoding',
+        'fileformat',
+        'filetype',
+      },
       lualine_y = { 'progress' },
       lualine_z = { 'location' }
     },
+    tabline = {
+      lualine_a = { 'buffers' },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {
+        {
+          'aerial',
+          sep = ' | '
+        },
+      },
+    },
+    extensions = { 'lazy' },
   },
 }
