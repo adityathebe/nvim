a28dd34980c3382f3b75dbd419fbe1515caf280a
Aditya Thebe <contact@adityathebe.com>
2024-10-23 15:22:40 +0545

    replace mini.statusline with lualine

---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,18 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    options = {
+      theme = 'auto',
+      globalstatus = true,
+    },
+    sections = {
+      lualine_a = { 'mode', 'buffers' },
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_c = { { 'filename', path = 1 } },
+      lualine_x = { 'encoding', 'fileformat', 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' }
+    },
+  },
+}
