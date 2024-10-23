e92deb0c129d73391feca03b9cecede2358f2ca7
Aditya Thebe <contact@adityathebe.com>
2024-10-23 14:13:04 +0545

    chore(aerial.nvim): always open it to the left side

--- lua/custom/plugins/aerial.lua
+++ lua/custom/plugins/aerial.lua
@@ -2,12 +2,19 @@ return {
   'stevearc/aerial.nvim',
   opts = {
     layout = {
+      default_direction = 'left',
+
       -- min_width and max_width can be a list of mixed types.
       -- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
       max_width = { 60, 0.2 },
-      min_width = 45,
+      min_width = 50,
     },
+
+    auto_jump = true,
+    lazy_load = false,
+    open_automatic = true,
   },
+  lazy = false,
   -- Optional dependencies
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
