f6b5dd01673de702cb324b0b12346d9557fa4e0d
Aditya Thebe <contact@adityathebe.com>
2024-10-24 19:43:02 +0545

    golines: don't format struct tags

--- lua/custom/plugins/format.lua
+++ lua/custom/plugins/format.lua
@@ -45,7 +45,7 @@ return { -- Autoformat
     },
     formatters = {
       -- ['goimports-reviser'] = { prepend_args = { '-rm-unused' } },
-      golines = { prepend_args = { '--max-len=180', '--tab-len=2', '--ignore-generated', '--chain-split-dots' } },
+      golines = { prepend_args = { '--max-len=180', '--tab-len=2', '--no-reformat-tags', '--ignore-generated', '--chain-split-dots' } },
     },
   },
 }
