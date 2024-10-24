1a47208cdc0cef37dcfc304349bfd97a605545c4
Aditya Thebe <contact@adityathebe.com>
2024-10-24 08:52:26 +0545

    add key mappings for vim-notify

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -3,13 +3,26 @@ return {
   event = "VeryLazy",
   opts = {
     presets = {
-      bottom_search = false, -- use a classic bottom cmdline for search
-      command_palette = true, -- position the cmdline and popupmenu together
+      bottom_search = false,        -- use a classic bottom cmdline for search
+      command_palette = true,       -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = false, -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = false, -- add a border to hover docs and signature help
+      inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = false,       -- add a border to hover docs and signature help
     },
   },
+  init = function()
+    vim.keymap.set("n", "<leader>nd", function()
+      require("noice").cmd("dismiss")
+    end)
+
+    vim.keymap.set("n", "<leader>nl", function()
+      require("noice").cmd("last")
+    end)
+
+    vim.keymap.set("n", "<leader>nh", function()
+      require("noice").cmd("history")
+    end)
+  end,
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
@@ -17,5 +30,5 @@ return {
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
     "rcarriga/nvim-notify",
-    }
+  }
 }
